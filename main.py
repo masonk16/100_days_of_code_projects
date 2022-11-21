7d45c79783cbb00367821ab0d13e4dff4567aa18
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-11-21 22:20:21 +0200

    Coffee machine initialized

--- main.py
+++ main.py
@@ -1,87 +1,58 @@
-#!/usr/bin/python3
-"""Place-Amenity API Routes"""
-from models import storage
-from flask import jsonify, request, abort
-from api.v1.views import app_views
-from models.review import Review
-from models.place import Place
-from models.user import User
-from models.amenity import Amenity
+@app_views.route("/places_search/", methods=["POST"], strict_slashes=False)
+def search_places():
+    places_list = []
+    place_dicts = []
+    cities_list = []
+    removal_list = []
+    empty = True
+    content = request.get_json(silent=True)
 
+    if type(content) is dict:
+        for key, value in content.items():
+            if len(content[key]) > 0:
+                empty = False
 
-@app_views.route("/places/<place_id>/amenities/", methods=["GET"], strict_slashes=False)
-def show_amenities_by_place(place_id):
-    """Retrieves the list of all Amenity objects of a Place.
-    Parameters:
-        place_id (str): the id of the place to display amenities
-    Returns:
-        A list of JSON dictionaries of all amenities in a 200 response
-    """
-    amenities_list = []
-    place = storage.get("Place", place_id)
-    if place:
-        for amenity in place.amenities:
-            amenities_list.append(amenity.to_dict())
-        return jsonify(amenities_list)
+        if len(content) == 0 or empty is True:
+            places = storage.all("Place").values()
+            for place in places:
+                place_dicts.append(place.to_dict())
+
+        if "states" in content:
+            for state in content["states"]:
+                state_obj = storage.get("State", state)
+                if state_obj:
+                    for city in state_obj.cities:
+                        cities_list.append(city)
+
+        if "cities" in content:
+            for city in content["cities"]:
+                city_obj = storage.get("City", city)
+                if city_obj:
+                    cities_list.append(city_obj)
+
+        for city in cities_list:
+            for place in city.places:
+                places_list.append(place)
+
+        if "amenities" in content:
+            for place in places_list:
+                for amenity in content["amenities"]:
+                    amenity_obj = storage.get("Amenity", amenity)
+                    if amenity_obj:
+                        if amenity_obj not in place.amenities:
+                            removal_list.append(place)
+                            break
+
+        for place in removal_list:
+            if place in places_list:
+                places_list.remove(place)
+
+        for place in places_list:
+            place_dicts.append(place.to_dict())
+
+        return jsonify(place_dicts)
+
     else:
-        abort(404)
-
-
-@app_views.route(
-    "/places/<place_id>/amenities/<amenity_id>",
-    methods=["DELETE"],
-    strict_slashes=False,
-)
-def delete_place_amenity(place_id, amenity_id):
-    """Deletes a Amenity object to a Place.
-    Parameters:
-        place_id (str): the id of the place
-        amenity_id (str): id of amenity to delete
-    Returns:
-        An empty JSON dictionary in a 200 response
-        A 404 response if the id does not match
-    """
-    place = storage.get("Place", place_id)
-    if place:
-        amenity = storage.get("Amenity", amenity_id)
-        if amenity:
-            if amenity in place.amenities:
-                place.amenities.remove(amenity)
-                storage.save()
-                return jsonify({})
-            else:
-                abort(404)
-        else:
-            abort(404)
-    else:
-        abort(404)
-
-
-@app_views.route(
-    "/places/<place_id>/amenities/<amenity_id>", methods=["POST"], strict_slashes=False
-)
-def create_place_amenity(place_id, amenity_id):
-    """Link a Amenity object to a Place.
-    Parameters:
-        place_id (str): the id of the place
-        amenity_id (str): id of the amenity
-    Returns:
-        A JSON dictionary of the amenity in a 201 response
-        A 404 response if not match for ids
-    """
-    place = storage.get("Place", place_id)
-    if place:
-        amenity = storage.get("Amenity", amenity_id)
-        if amenity:
-            if amenity in place.amenities:
-                return jsonify(amenity.to_dict())
-            else:
-                place.amenities.append(amenity)
-                storage.save()
-                response = jsonify(amenity.to_dict())
-                response.status_code = 201
-                return response
-        else:
-            abort(404)
-    else:
-        abort(404)
+        response = jsonify({"error": "Not a JSON"})
+        response.status_code = 400
+        return response
