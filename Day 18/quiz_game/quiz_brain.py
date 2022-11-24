d4866007283f818a01506734533cb7bff2513fa6
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-11-24 22:37:00 +0200

    check answer method added

--- Day 18/quiz_game/quiz_brain.py
+++ Day 18/quiz_game/quiz_brain.py
@@ -2,6 +2,7 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        self.score = 0
 
     def still_has_questions(self):
         num_of_questions = len(self.question_list)
@@ -10,4 +11,12 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        user_response = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        self.check_answer(user_response, current_question.answer)
+
+    def check_answer(self, user_response, correct_answer):
+        if user_response.lower() == correct_answer.lower():
+            print("You got it right!")
+        else:
+            print("You got it wrong.")
+        print(f"The correct answer was: {correct_answer}.")
