2262bd7d3181ddaa6679504d5d4e9dfd6cb0dfc5
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-11-24 22:30:03 +0200

    question counter

--- Day 18/quiz_game/quiz_brain.py
+++ Day 18/quiz_game/quiz_brain.py
@@ -3,7 +3,11 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = question_list
 
+    def still_has_questions(self):
+        num_of_questions = len(self.question_list)
+        return self.question_number < num_of_questions
+
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False)")
+        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
