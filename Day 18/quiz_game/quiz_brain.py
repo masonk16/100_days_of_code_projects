528236e251943dc55c3d67338c16fcc0368a812f
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-11-24 22:20:12 +0200

    import and initialize quizbrain

--- Day 18/quiz_game/quiz_brain.py
+++ Day 18/quiz_game/quiz_brain.py
@@ -1,8 +1,9 @@
 class QuizBrain:
-    def __int__(self, question_list):
+    def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
-        input(f"Q.{self.question_number}: {current_question.text}. (True/False)")
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {current_question.text} (True/False)")
