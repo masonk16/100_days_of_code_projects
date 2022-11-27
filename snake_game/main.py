2af28d52614238fa6a91787a058adc83ef00a0d5
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-11-27 20:58:56 +0200

    Add movement methods to snake class

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,19 +1,25 @@
 from turtle import Screen
-import time
 from snake import Snake
+import time
 
 screen = Screen()
-screen.setup(width=600, height=400)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Game")
+screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
 
-play_game = True
-while play_game:
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
     screen.update()
-    time.sleep(1)
+    time.sleep(0.1)
 
     snake.move()
 
