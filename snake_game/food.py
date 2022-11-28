4d367646acaa346f4ef22e47e240623d11713e30
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-11-28 20:45:02 +0200

    fix food size

--- snake_game/food.py
+++ snake_game/food.py
@@ -7,8 +7,8 @@ class Food(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("circle")
-        self.pen()
-        self.pensize(stretch_len=0.5, stretch_wid=0.5)
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("red")
         self.speed("fastest")
         random_x = random.randint(-290, 290)
