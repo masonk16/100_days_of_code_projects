381a412b25505a942639d225ce8258e604a2323e
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-11-28 21:30:41 +0200

    add game over method to scoreboard

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -1,7 +1,6 @@
 from turtle import Turtle
 ALIGNMENT = "center"
-FONT = "Arial"
-SIZE = ('Arial', 20, 'normal')
+FONT = ('Arial', 20, 'normal')
 
 
 class Scoreboard(Turtle):
@@ -11,12 +10,17 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
-        self.goto(x=0, y=270)
+        self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over!", align=ALIGNMENT, font=FONT)
+
 
     def increase_score(self):
         self.score += 1
