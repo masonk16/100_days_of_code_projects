53b03c36ce1c037867f705982d622d09fb817b82
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-11-28 20:39:55 +0200

    import food to main game

--- snake_game/food.py
+++ snake_game/food.py
@@ -1,7 +1,16 @@
 from turtle import Turtle
+import random
 
 
 class Food(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.shape("circle")
+        self.pen()
+        self.pensize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("red")
+        self.speed("fastest")
+        random_x = random.randint(-290, 290)
+        random_y = random.randint(-290, 290)
+        self.goto(random_x, random_y)
