4b88ad6bf2d542f72fe308e2c99d9e063b65051e
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-11-29 20:55:53 +0200

    create paddle class

--- the _pong_game/main.py
+++ the _pong_game/main.py
@@ -1,5 +1,5 @@
 from turtle import Turtle, Screen
-from paddle import PaddleOne
+from paddle import Paddle
 
 
 screen = Screen()
@@ -8,31 +8,18 @@ screen.setup(width=800, height=600)
 screen.title("The Pong Game")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.speed("fastest")
-paddle.penup()
-paddle.goto(350, 0)
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-
-paddle.penup()
-paddle.goto(350, 0)
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+right_paddle = Paddle(350, 0)
+left_paddle = Paddle(-350, 0)
 
 screen.listen()
-screen.onkey(go_up, "Up")
+screen.onkey(right_paddle.go_up, "Up")
+screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
 
 play_game = True
 while play_game:
     screen.update()
 
-     screen.exitonclick()
+    screen.exitonclick()
 
--- the _pong_game/paddle.py
+++ the _pong_game/paddle.py
@@ -1,13 +1,20 @@
 from turtle import Turtle
 
 
-class PaddleOne(Turtle):
+class Paddle(Turtle):
     """Instantiates the first paddle."""
-    def __init__(self):
+    def __init__(self, x_axis, y_axis):
         super().__init__()
         self.shape("square")
-        self.shapesize(stretch_wid=20, stretch_len=100)
+        self.shapesize(stretch_wid=5, stretch_len=1)
         self.color("white")
         self.penup()
-        self.goto(350, 0)
+        self.goto(x_axis, y_axis)
 
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
