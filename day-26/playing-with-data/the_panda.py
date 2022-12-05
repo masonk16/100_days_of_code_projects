0b1dd0ad63b4dedf6d1f0f9b77748a441d88b78a
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-05 22:22:00 +0200

    reading data from csv using pandas

--- day-26/playing-with-data/the_panda.py
+++ day-26/playing-with-data/the_panda.py
@@ -1,5 +1,31 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
+# print(type(data))
+# print(data["temp"])
 
-print(data["temp"])
+data_dict = data.to_dict()
+
+temp_list = data["temp"].to_list()
+
+# print(data["temp"].mean())
+max_temp = data["temp"].max()
+
+# Get data in columns
+# print(data.condition)
+
+# Get data in row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == max_temp])
+
+monday = data[data.day == "Monday"]
+fah = (monday.temp * 1.8) + 32
+print(fah)
+
+# Create data from scratch
+student_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data = pandas.DataFrame(student_dict)
+data.to_csv("students.csv")
