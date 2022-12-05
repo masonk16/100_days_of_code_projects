3a8e152bc5965d3018d88aa24a38dc8aaee30232
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-05 21:35:59 +0200

    Csv reader module

---
+++ day-26/playing-with-data/main.py
@@ -0,0 +1,12 @@
+# with open("weather_data.csv", 'r') as weather_file:
+#     data = weather_file.readlines()
+
+import csv
+with open("weather_data.csv", 'r') as weather_file:
+    data = csv.reader(weather_file)
+    temperatures = []
+    for row in data:
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+
+    print(temperatures)
