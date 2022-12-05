d08a6658276a5ff62e0e7529482e7effb960c8e9
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-05 22:51:34 +0200

    creating a dataframe

--- day-26/playing-with-data/the_panda.py
+++ day-26/playing-with-data/the_panda.py
@@ -1,31 +1,14 @@
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-# print(type(data))
-# print(data["temp"])
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
-data_dict = data.to_dict()
+gray_squirrels = len(data[data["Primary Fur Color"] == "Gray"])
+cin_squirrels = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels = len(data[data["Primary Fur Color"] == "Black"])
 
-temp_list = data["temp"].to_list()
-
-# print(data["temp"].mean())
-max_temp = data["temp"].max()
-
-# Get data in columns
-# print(data.condition)
-
-# Get data in row
-# print(data[data.day == "Monday"])
-# print(data[data.temp == max_temp])
-
-monday = data[data.day == "Monday"]
-fah = (monday.temp * 1.8) + 32
-print(fah)
-
-# Create data from scratch
-student_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
+data_dict = {
+    "Fur Color": ["Black", "Cinnamon", "Gray"],
+    "Count": [black_squirrels, cin_squirrels, gray_squirrels]
 }
-data = pandas.DataFrame(student_dict)
-data.to_csv("students.csv")
+df = pandas.DataFrame(data_dict)
+df.to_csv("Primary_fur_count.csv")
