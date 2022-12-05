cd5b085556969192d0461f689e9ebd705cf6478a
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-05 23:42:32 +0200

    Add csv generation for missing states

--- day-26/us_states_game/main.py
+++ day-26/us_states_game/main.py
@@ -20,6 +20,14 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer = screen.textinput(title=f"{len(guessed_states)}/50 States Correct.", prompt="Enter a state name: ").title()
 
+    if answer == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer in all_states:
         guessed_states.append(answer)
         t = turtle.Turtle()
