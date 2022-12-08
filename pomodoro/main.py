79667d9bd0004b1bc7726e38467ec8dd78ef97f9
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-08 23:17:56 +0200

    Set timer logic

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -6,25 +6,42 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
-    countdown(5 * 60)
+    global reps
+    reps += 1
+    work = WORK_MIN * 60
+    short_break = SHORT_BREAK_MIN * 60
+    long_break = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        countdown(long_break)
+        reps = 0
+    elif reps % 2 == 0:
+        countdown(short_break)  
+    else:
+        countdown(work)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 def countdown(count):
     count_minute = math.floor(count / 60)
     count_seconds = count % 60
+    if count_seconds < 10:
+        count_seconds = f"0{count_seconds}"
     canvas.itemconfig(timer_text, text=f"{count_minute}:{count_seconds}")
     if count > 0:
         window.after(1000, countdown, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
