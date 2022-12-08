d132c712f4bd652cb64585a220562871dd3843ed
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-08 22:28:37 +0200

    Create countdown timer

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -13,7 +13,15 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    countdown(5 * 60)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def countdown(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, countdown, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -24,13 +32,13 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 domasi = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=domasi)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 40, "bold"))
 timer_label.grid(column=1, row=0)
 
-start_button = Button(text="Start", width=10)
+start_button = Button(text="Start", width=10, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", width=10)
