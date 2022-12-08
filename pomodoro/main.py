f251c75b1cf09d444a0a7a776ffd3e07b5e3ae4c
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-08 20:49:33 +0200

    UI steup done

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,27 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+domasi = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=domasi)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 40, "bold"))
+timer_label.grid(column=1, row=0)
+
+start_button = Button(text="Start", width=10)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", width=10)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
+window.mainloop()
