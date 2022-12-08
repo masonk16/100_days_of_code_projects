5aefc3a78e870f3389db3f70b7f92d00a9f1a544
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-08 20:16:46 +0200

    Pomodoro initialization

--- day-26/us_states_game/main.py
+++ day-26/us_states_game/main.py
@@ -22,9 +22,7 @@ while len(guessed_states) < 50:
 
     if answer == "Exit":
         missing_states = [state for state in all_states if state not in guessed_states]
-        # for state in all_states:
-        #     if state not in guessed_states:
-        #         missing_states.append(state)
+        #  
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
