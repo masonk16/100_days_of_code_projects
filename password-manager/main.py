8aff32d91f7d2820a999aa7dc1c6b27198bf5f54
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-09 21:46:36 +0200

    Add save function

--- password-manager/main.py
+++ password-manager/main.py
@@ -3,6 +3,20 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    """
+    Saves the new password as a new line in the file when the Add Password button is clicked.
+
+    Clears all fields.
+    """
+    with open("svombonorodzenyu.txt", "a") as masvombonoro:
+        website = website_input.get()
+        email = email_input.get()
+        password = password_input.get()
+        masvombonoro.write(f"{website} | {email} | {password}\n")
+
+    website_input.delete(0, "end")
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -19,6 +33,7 @@ website_label = Label(text="Website:", pady=10)
 website_label.grid(column=0, row=1)
 
 website_input = Entry(width=55)
+website_input.focus()
 website_input.grid(column=1, row=1, columnspan=2)
 
 # Email
@@ -26,6 +41,7 @@ email_label = Label(text="Email/Username:", pady=10)
 email_label.grid(column=0, row=2)
 
 email_input = Entry(width=55)
+email_input.insert(0, "kudyausavi@gmail.com")
 email_input.grid(column=1, row=2, columnspan=2)
 
 # Password
@@ -38,7 +54,7 @@ password_input.grid(column=1, row=3)
 gen_pass_button = Button(text="Generate Password")
 gen_pass_button.grid(column=2, row=3)
 
-add_pass_button = Button(text="Add Password", width=46)
+add_pass_button = Button(text="Add Password", width=46, command=save)
 add_pass_button.grid(column=1, row=4, columnspan=2)
 
 
