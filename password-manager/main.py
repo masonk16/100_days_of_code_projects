f91ce66f23cb6953b0561e2a027c32960eb3597d
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-09 21:25:19 +0200

    Adjust sizes

--- password-manager/main.py
+++ password-manager/main.py
@@ -7,35 +7,38 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 svombonoro = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=svombonoro)
 canvas.grid(column=1, row=0)
 
+# Website
 website_label = Label(text="Website:", pady=10)
 website_label.grid(column=0, row=1)
 
-website_input = Entry(width=35)
+website_input = Entry(width=55)
 website_input.grid(column=1, row=1, columnspan=2)
 
+# Email
 email_label = Label(text="Email/Username:", pady=10)
 email_label.grid(column=0, row=2)
 
-email_input = Entry(width=35)
+email_input = Entry(width=55)
 email_input.grid(column=1, row=2, columnspan=2)
 
+# Password
 password_label = Label(text="Password:", pady=10)
 password_label.grid(column=0, row=3)
 
-password_input = Entry(width=21)
+password_input = Entry(width=35)
 password_input.grid(column=1, row=3)
 
 gen_pass_button = Button(text="Generate Password")
 gen_pass_button.grid(column=2, row=3)
 
-add_pass_button = Button(text="Add Password", width=36)
+add_pass_button = Button(text="Add Password", width=46)
 add_pass_button.grid(column=1, row=4, columnspan=2)
 
 
