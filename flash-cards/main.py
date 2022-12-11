11cae1c67317233cb218ccdc24d97ee0f3f29f95
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-11 19:56:15 +0200

    Call word on UI startup

--- flash-cards/main.py
+++ flash-cards/main.py
@@ -5,14 +5,14 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 
 #>>>> WORD DATA <<<<<#
-def select_word():
-    words = pandas.read_csv("data/french_words.csv")
-    word_dict = words.to_dict(orient="records")
+words = pandas.read_csv("data/french_words.csv")
+word_dict = words.to_dict(orient="records")
 
+def select_word():
     current_word = random.choice(word_dict)
     french_word = current_word["French"]
-    
-    canvas.itemconfig(word_text, text=french_word)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=french_word)
 
 
 # >>>>UI SETUP<<<<< #
@@ -23,8 +23,8 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=528, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_img = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 264, image=front_img)
-canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
-word_text = canvas.create_text(400, 264, text="trouve", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 264, text="word", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Wrong button
@@ -37,5 +37,6 @@ right_img = PhotoImage(file="./images/right.png")
 right_button = Button(image=right_img, highlightthickness=0, command=select_word)
 right_button.grid(column=1, row=1)
 
+select_word()
 
 window.mainloop()
