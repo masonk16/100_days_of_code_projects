f625bf5fbcc867faa455706fd7a731eceb9961b6
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-11 19:09:10 +0200

    add buttons

--- flash-cards/main.py
+++ flash-cards/main.py
@@ -11,4 +11,16 @@ flash_bg = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 260, image=flash_bg)
 canvas.grid(column=0, row=0, columnspan=2)
 
+# Wrong button
+wrong_img = PhotoImage(file="./images/wrong.png")
+wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button.grid(column=0, row=1)
+
+
+# Right button
+right_img = PhotoImage(file="./images/right.png")
+right_button = Button(image=right_img, highlightthickness=0)
+right_button.grid(column=1, row=1)
+
+
 window.mainloop()
