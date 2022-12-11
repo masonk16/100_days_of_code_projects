ae5d99aa2342faea7ab8a223953b62520ee4a848
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-11 19:01:46 +0200

    Add card background

--- flash-cards/main.py
+++ flash-cards/main.py
@@ -4,9 +4,11 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=800, height=528, bg=BACKGROUND_COLOR)
-flash_bg = PhotoImage(file="/images/card_front.png")
-canvas.create_image(400, 200, image=flash_bg)
+canvas = Canvas(width=800, height=528, bg=BACKGROUND_COLOR, highlightthickness=0)
+flash_bg = PhotoImage(file="./images/card_front.png")
+canvas.create_image(400, 260, image=flash_bg)
+canvas.grid(column=0, row=0, columnspan=2)
 
 window.mainloop()
