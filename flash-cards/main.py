b89a6f3c9b142d9df8893a05f62dc6a9dd90aea2
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-11 19:27:33 +0200

    Read CSV File

--- flash-cards/main.py
+++ flash-cards/main.py
@@ -1,23 +1,30 @@
 from tkinter import *
+import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+#>>>> WORD DATA <<<<<#
+words = pandas.read_csv("data/french_words.csv")
+#print(words)
+
+
+
+# >>>>UI SETUP<<<<< #
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=528, bg=BACKGROUND_COLOR, highlightthickness=0)
-flash_bg = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 260, image=flash_bg)
+front_img = PhotoImage(file="./images/card_front.png")
+canvas.create_image(400, 264, image=front_img)
 canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="trouve", font=("Ariel", 60, "bold"))
+canvas.create_text(400, 264, text="trouve", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Wrong button
 wrong_img = PhotoImage(file="./images/wrong.png")
 wrong_button = Button(image=wrong_img, highlightthickness=0)
 wrong_button.grid(column=0, row=1)
-
 
 # Right button
 right_img = PhotoImage(file="./images/right.png")
