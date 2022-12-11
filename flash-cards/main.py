e85c46cbf38422a41ced315fd0832bd9a68f96f4
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-11 19:44:53 +0200

    Select random French word on button press

--- flash-cards/main.py
+++ flash-cards/main.py
@@ -1,12 +1,18 @@
 from tkinter import *
 import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
 #>>>> WORD DATA <<<<<#
-words = pandas.read_csv("data/french_words.csv")
-#print(words)
+def select_word():
+    words = pandas.read_csv("data/french_words.csv")
+    word_dict = words.to_dict(orient="records")
 
+    current_word = random.choice(word_dict)
+    french_word = current_word["French"]
+    
+    canvas.itemconfig(word_text, text=french_word)
 
 
 # >>>>UI SETUP<<<<< #
@@ -18,17 +24,17 @@ canvas = Canvas(width=800, height=528, bg=BACKGROUND_COLOR, highlightthickness=0
 front_img = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 264, image=front_img)
 canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 264, text="trouve", font=("Ariel", 60, "bold"))
+word_text = canvas.create_text(400, 264, text="trouve", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Wrong button
 wrong_img = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=select_word)
 wrong_button.grid(column=0, row=1)
 
 # Right button
 right_img = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_img, highlightthickness=0)
+right_button = Button(image=right_img, highlightthickness=0, command=select_word)
 right_button.grid(column=1, row=1)
 
 
