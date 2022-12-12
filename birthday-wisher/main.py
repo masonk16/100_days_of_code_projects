a4acecf61928001238e958658323d8cecd195c92
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-12 23:02:13 +0200

    Create todays date tuple

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -2,7 +2,9 @@ import pandas
 import datetime as dt
 import random
 import smtplib
-##################### Extra Hard Starting Project ######################
+
+EMAIL = "mabasics175@gmail.com"
+PASSWORD = "wllaqeaotxnaitmu"
 
 # 1. Update the birthdays.csv
 
@@ -14,9 +16,9 @@ birthdays = pandas.read_csv("birthdays.csv")
 now = dt.datetime.now()
 day = now.day
 month = now.month
+today = (month, day)
 
-birth_day = birthdays[birthdays["day"] == 2]
-birth_month = birthdays[birthdays["day"] == 7]
+
 
 letters = [
     "letter_templates/letter_1.txt",
@@ -28,12 +30,18 @@ if birth_day is not None and birth_month is not None:
     with open(random.choice(letters), "r") as letter:
         contents = letter.read()
         name =
-        message = contents.replace("[NAME}", name)
+        birthday_message = contents.replace("[NAME}", name)
 
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    connection.starttls()
+    connection.login(EMAIL, PASSWORD)
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
+    # Send email with birthday_message as message body.
+    connection.sendmail(
+        from_addr=EMAIL,
+        to_addrs="grey.pafotisevheni@yahoo.com",
+        msg=f"Subject: Happy Birthday\n\n {birthday_message}"
+    )
 
 
 
