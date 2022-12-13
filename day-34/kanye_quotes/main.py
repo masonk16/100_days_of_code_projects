37010d582d5baace0c448d445310224cdf81b8c4
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-13 20:02:33 +0200

    Using the sunrise API

--- day-34/kanye_quotes/main.py
+++ day-34/kanye_quotes/main.py
@@ -4,10 +4,10 @@ import requests
 
 def get_quote():
     response = requests.get("https://api.kanye.rest")
+    response.raise_for_status()
     data = response.json()
     quote = data["quote"]
     canvas.itemconfig(quote_text, text=quote)
-
 
 
 window = Tk()
@@ -23,7 +23,6 @@ canvas.grid(row=0, column=0)
 kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
-
 
 
 window.mainloop()
--- day-34/main.py
+++ day-34/main.py
@@ -1,12 +1,24 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+LAT = -17.766150
+LONG = 30.979220
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# latitude = data["iss_position"]["latitude"]
+# longitude = data["iss_position"]["longitude"]
+#
+# iss_position = (latitude, longitude)
+# print(iss_position)
+
+parameters = {
+    "lat": LAT,
+    "lng": LONG,
+}
+response = requests.get("https://api.sunrise-sunset.org/json",
+                        params=parameters)
 response.raise_for_status()
-
 data = response.json()
-
-latitude = data["iss_position"]["latitude"]
-longitude = data["iss_position"]["longitude"]
-
-iss_position = (latitude, longitude)
-print(iss_position)
+print(data)
