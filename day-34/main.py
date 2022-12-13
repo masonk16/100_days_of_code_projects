458f01ae7ab9d1841dc26a8163ff6219362dc8cc
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-13 20:13:43 +0200

    Compare both times

--- day-34/main.py
+++ day-34/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 LAT = -17.766150
 LONG = 30.979220
@@ -16,9 +17,17 @@ LONG = 30.979220
 parameters = {
     "lat": LAT,
     "lng": LONG,
+    "formatted": 0
 }
 response = requests.get("https://api.sunrise-sunset.org/json",
                         params=parameters)
 response.raise_for_status()
 data = response.json()
-print(data)
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+print(time_now.hour)
