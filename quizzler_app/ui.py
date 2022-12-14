0f3832eacc55e0f481e52f59c7a80fd482c15722
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-14 20:10:04 +0200

    Setup UI class with tkinter

--- quizzler_app/ui.py
+++ quizzler_app/ui.py
@@ -1,2 +1,28 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.label = Label(text="Score: 0", bg=THEME_COLOR, fg="white", font=("Arial", 14, "normal"))
+        self.label.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.canvas.create_text(150, 125, text="Question goes here.", font=("Arial", 20, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2)
+
+        true_mark = PhotoImage(file="images/true.png")
+        self.true_btn = Button(image=true_mark, highlightthickness=0,  padx=20, pady=20)
+        self.true_btn.grid(column=0, row=2)
+
+        false_mark = PhotoImage(file="images/false.png")
+        self.false_btn = Button(image=false_mark, highlightthickness=0, padx=20, pady=20)
+        self.false_btn.grid(column=1, row=2)
+
+        self.window.mainloop()
