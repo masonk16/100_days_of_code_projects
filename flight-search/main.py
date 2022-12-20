1d4ff7516698ca60424c4b12a882bcb9df9468e6
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-20 16:38:18 +0200

    Compare flights against prices

--- flight-search/main.py
+++ flight-search/main.py
@@ -1,12 +1,15 @@
 from datetime import datetime, timedelta
 from data_manager import DataManager
 from flight_search import FlightSearch
+from notification_manager import NotificationManager
 
 data_manager = DataManager()
-sheet_data = data_manager.get_destination_data()
 flight_search = FlightSearch()
+notification_manager = NotificationManager()
 
 ORIGIN_CITY_IATA = "HRE"
+
+sheet_data = data_manager.get_destination_data()
 
 if sheet_data[0]["iataCode"] == "":
     from flight_search import FlightSearch
@@ -28,3 +31,7 @@ for destination in sheet_data:
         from_time=tomorrow,
         to_time=six_month_from_today
     )
+    if flight.price < destination["lowestPrice"]:
+        notification_manager.send_sms(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        )
