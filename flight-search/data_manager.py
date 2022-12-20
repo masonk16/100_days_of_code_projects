9c1fe25e3ca7e069923788e98966ac7cd4ea81e5
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-20 16:07:13 +0200

    Create destination data method

--- flight-search/data_manager.py
+++ flight-search/data_manager.py
@@ -1,3 +1,19 @@
+import requests
+from pprint import pprint
+
+SHEETY_PRICES_ENDPOINT= "https://api.sheety.co/5a2e01fafcc55f932af52f3e9867cb60/flightDeals/prices")
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    """
+    This class is responsible for talking to the Google Sheet.
+    """
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
