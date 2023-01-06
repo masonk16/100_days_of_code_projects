d64f4d6ec9c5f1f0cd1a4c5665fed39618e2b577
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-01-06 22:04:32 +0200

    Use element attributes to extract data

--- beautiful-soup/main.py
+++ beautiful-soup/main.py
@@ -7,7 +7,12 @@ soup = BeautifulSoup(contents, 'html.parser')
 
 all_anchor_tags = soup.find_all(name="a")
 
-for tag in all_anchor_tags:
-    print(tag.getText())
-    print(tag.get("href"))
+# for tag in all_anchor_tags:
+#     # print(tag.getText())
+#     # print(tag.get("href"))
 
+heading = soup.find(name="h1", id="name")
+# print(heading) 
+
+h3_heading = soup.find_all("h3", class_="heading")
+print(h3_heading)
