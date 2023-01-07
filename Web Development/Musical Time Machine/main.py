91836afe16db3aff9f77391555ffd133f04e8abb
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-01-07 22:01:41 +0200

    Setup spotify API

--- Web Development/Musical Time Machine/main.py
+++ Web Development/Musical Time Machine/main.py
@@ -1,26 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 import os
 from dotenv import load_dotenv
 
-SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_SECRET')
-SPOTIFY_CLIENT_SECRET = os.environ.get('spotify_client_secret')
-
 load_dotenv()
 
-date = input("Which date would you like to travel to? YYYY-MM-DD: ")
+SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 
-billboard_url = f"https://www.billboard.com/charts/hot-100/{date}"
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=SPOTIFY_CLIENT_ID,
+        client_secret=SPOTIFY_CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
 
-response = requests.get(billboard_url)
-songs_html = response.text
-
-soup = BeautifulSoup(songs_html, "html.parser")
-song_title_tags = soup.find_all("h3", class_="a-no-trucate")
-# song_artist = soup.find(name="span", class_="a-no-trucate").getText()
-
-song_titles = [" ".join(song.getText().split()) for song in song_title_tags]
-
-
-print(song_titles)
-# print(song_artist)
+# date = input("Which date would you like to travel to? YYYY-MM-DD: ")
+#
+# billboard_url = f"https://www.billboard.com/charts/hot-100/{date}"
+#
+# response = requests.get(billboard_url)
+# songs_html = response.text
+#
+# soup = BeautifulSoup(songs_html, "html.parser")
+# song_title_tags = soup.find_all("h3", class_="a-no-trucate")
+# # song_artist = soup.find(name="span", class_="a-no-trucate").getText()
+#
+# song_titles = [" ".join(song.getText().split()) for song in song_title_tags]
+#
+#
+# print(song_titles)
+# # print(song_artist)
