792302d63bb9f0f479a972083a3d1df35bf801f2
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-01-09 21:00:52 +0200

    Scrape title of desired product.

--- Web Development/Amazon Price Tracker/main.py
+++ Web Development/Amazon Price Tracker/main.py
@@ -1,5 +1,11 @@
 import requests
+import lxml
 from bs4 import BeautifulSoup
+import smtplib
+
+
+TARGET_PRICE = 405.00
+
 
 # URL and headers for Amazon product page
 url = "https://www.amazon.com/SAMSUNG-3840x2160-Computer-border-less-LU32R590CWNXZA/dp/B07L9G1BFX/ref=sr_1_3?crid=130JAPGMTJUYA&keywords=curved%2Bmonitor%2B4k&qid=1673289436&refinements=p_n_size_browse-bin%3A3547808011%2Cp_89%3ADell%7CSAMSUNG%2Cp_n_condition-type%3A2224371011&rnid=2224369011&s=pc&sprefix=curved%2Bmonitor%2B4k%2Caps%2C487&sr=1-3&th=1"
@@ -14,5 +20,11 @@ response = requests.get(url=url, headers=headers)
 # Make soup and scrape price
 soup = BeautifulSoup(response.content, "lxml")
 price_tag = (soup.find("span", class_="a-offscreen").getText()).split("$")
-price = float(price_tag[1])
-print(price)
+current_price = float(price_tag[1])
+
+# Get product title
+title = (soup.select_one(selector="h1 span", id="title").getText()).strip()
+print(title)
+
+# Check if current price is below target price
+# if current_price <= TARGET_PRICE:
