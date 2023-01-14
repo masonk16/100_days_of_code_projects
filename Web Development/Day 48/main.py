3e9ce639ac742d47b620721a2b1d77ea7a8765b0
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-01-14 21:53:55 +0200

    Use Selenium4 compliance

--- Web Development/Day 48/main.py
+++ Web Development/Day 48/main.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 load_dotenv()
 
 CHROME_DRIVER_PATH = os.getenv('CHROME_DRIVER_PATH')
-driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-
+driver.get("https://www.amazon.com")
