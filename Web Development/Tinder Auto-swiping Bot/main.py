a0d1927e3e44043c49101ed90e6ca41d8bd98b08
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-01-16 23:16:10 +0200

    Trying with Google Log in

--- Web Development/Tinder Auto-swiping Bot/main.py
+++ Web Development/Tinder Auto-swiping Bot/main.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -31,23 +33,34 @@ login_btn.click()
 
 time.sleep(5)
 
-facebook_btn = driver.find_element(By.XPATH, '//*[@id="c-1480665743"]/main/div/div[1]/div/div/div[3]/span/div[2]/button')
-facebook_btn.click()
+# facebook_btn = driver.find_element(By.XPATH, '//*[@id="c-1480665743"]/main/div/div[1]/div/div/div[3]/span/div[2]/button')
+# facebook_btn.click()
+
+# driver.switch_to.frame('gsi_984808_861630')
+WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="gsi_984808_861630"]')))
+
+google_continue = driver.find_element(By.XPATH, '//*[@id="container"]/div/div[2]/span[1]')
+google_continue.click()
 
 time.sleep(2)
 
 base_window = driver.window_handles[0]
 fb_login_window = driver.window_handles[1]
+
 driver.switch_to.window(fb_login_window)
 print(driver.title)
 
 time.sleep(3)
 
-phone_num = driver.find_element(By.XPATH, '//*[@id="email"]')
-phone_num.send_keys(PHONE_NUMBER)
-password = driver.find_element(By.NAME, "pass")
-password.send_keys(PASSWORD)
+# phone_num = driver.find_element(By.XPATH, '//*[@id="email"]')
+# phone_num.send_keys(PHONE_NUMBER)
+# password = driver.find_element(By.NAME, "pass")
+# password.send_keys(PASSWORD)
+#
+# fb_login = driver.find_element(By.NAME, "login")
+# fb_login.click()
+#
+# time.sleep(10)
 
-fb_login = driver.find_element(By.NAME, "login")
-fb_login.click()
+
 
