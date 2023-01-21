6c114069a45ec7ec01dcfc51ee668201f20b30cf
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-01-21 21:57:34 +0200

    Testing out decorators in python

---
+++ Web Development/Hello Flask/test_decorators.py
@@ -0,0 +1,31 @@
+import time
+
+current_time = time.time()
+print(current_time)
+
+
+def speed_calc_decorator(function):
+    def wrapper_function():
+        start = time.time()
+        function()
+        end = time.time()
+        run_speed = end - start
+        print(f'{function.__name__} run speed: {run_speed}s')
+
+    return wrapper_function
+
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(10000000):
+        i * i
+
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(100000000):
+        i * i
+
+
+fast_function()
+slow_function()
