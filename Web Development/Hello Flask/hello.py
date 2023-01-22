d7c2af3a21ad80d16a58acbd5b61f620be944d90
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-01-22 21:11:52 +0200

    Rendering HTMl in the return function.

--- Web Development/Hello Flask/hello.py
+++ Web Development/Hello Flask/hello.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    # Rendering HTML within the return
+    return '<h1 style="text-align: center; ">Hello, World!</h1>' \
+           '<p>This is a paragraph.</p>' \
+           '<img src="https://i.pinimg.com/originals/e6/0a/b2/e60ab23aa17dff0db3e76312bb4d7ec2.jpg" style="margin: auto;">' \
+           '<img src="https://media.giphy.com/media/8GehNdh9Cj6es/giphy.gif">'
 
 
 @app.route('/bye')
@@ -13,10 +17,12 @@ def say_bye():
     return 'Bye!'
 
 
+# Creating variable paths and converting the path to a specified data type
 @app.route("/<string:name>/<int:number>")
 def greeting(name, number):
     return f'Hello {name}, you are {number} years old!'
 
 
 if __name__ == "__main__":
+    # Run app in debug mode to auto-reload and access Flask debugger.
     app.run(debug=True)
