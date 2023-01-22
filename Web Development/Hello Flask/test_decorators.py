2b3a7ed895669640ea63c624dc1e044d432dd5f6
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-01-22 21:19:53 +0200

    Advanced decorators.

--- Web Development/Hello Flask/test_decorators.py
+++ Web Development/Hello Flask/test_decorators.py
@@ -29,3 +29,17 @@ def slow_function():
 
 fast_function()
 slow_function()
+
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post.")
+
+
+new_user = User("Mason")
+create_blog_post(new_user)
