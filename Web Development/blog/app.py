efa6a9c41b5c356751df6d702d3c41c2a9f4ab47
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-01-24 21:52:01 +0200

    Create dynamic route for single blog post

--- Web Development/blog/app.py
+++ Web Development/blog/app.py
@@ -12,5 +12,17 @@ def home():
     return render_template("index.html", posts=all_posts)
 
 
+@app.route('/post/<int:blog_id>')
+def get_blog(blog_id):
+    blogs_url = ' https://api.npoint.io/c790b4d5cab58020d391'
+    response = requests.get(blogs_url)
+    all_posts = response.json()
+    requested_post = None
+    for blog_post in all_posts:
+        if blog_post.id == blog_id:
+            requested_post = blog_post
+    return render_template('post.html', post=requested_post)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
