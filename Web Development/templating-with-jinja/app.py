a2c2c2eedb3e1c14c55efdb9b5a6daa8f1832f59
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-01-24 20:52:58 +0200

    Create guess URL route

---
+++ Web Development/templating-with-jinja/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import random
+import datetime
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    random_number = random.randint(1, 10)
+    current_year = datetime.datetime.now().year
+    return render_template('index.html', random_number=random_number, year=current_year)
+
+
+@app.route('/guess/<string:name>')
+def guess(name):
+    gender_response = requests.get(f'https://api.genderize.io?name={name}')
+    gender = gender_response.json()['gender']
+    age_response = requests.get(f'https://api.agify.io?name={name}')
+    age = age_response.json()['age']
+
+    return render_template('guessing.html', name=name, gender=gender, age=age)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
