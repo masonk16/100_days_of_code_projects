0427d611d3450f70ac1e907e81678be9fd0801d3
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-01-29 20:49:07 +0200

    Get form data

--- Web Development/html-forms/app.py
+++ Web Development/html-forms/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def receive_data():
+    if request.method == "POST":
+        name = request.form['name']
+        password = request.form['password']
+        return render_template('logged_in.html', name=name, password=password)
 
 
 if __name__ == '__main__':
