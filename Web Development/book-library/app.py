156af9d68e84e5f9564bfe623942d5dbfa0e8fff
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-02-01 20:39:48 +0200

    Create books table in database

--- Web Development/book-library/app.py
+++ Web Development/book-library/app.py
@@ -6,30 +6,32 @@ app = Flask(__name__)
 
 db = sqlite3.connect("books-collection.db")
 cursor = db.cursor()
-
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)"
+)
 
 
 all_books = []
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html', books=all_books)
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=['GET', 'POST'])
 def add():
     if request.method == "POST":
         new_book = {
             "title": request.form["title"],
             "author": request.form["author"],
-            "rating": request.form["rating"]
+            "rating": request.form["rating"],
         }
         all_books.append(new_book)
 
-        return redirect(url_for('home'))
+        return redirect(url_for("home"))
 
-    return render_template('add.html')
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
