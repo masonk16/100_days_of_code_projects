bceb3432e7c9abbd273d1d16a4e565ce6649500a
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-02-04 21:06:03 +0200

    Use dictionary comprehension to convert response to JSON

--- Web Development/cafe-api/app.py
+++ Web Development/cafe-api/app.py
@@ -23,6 +23,10 @@ class Cafe(db.Model):
     has_sockets = db.Column(db.Boolean, nullable=False)
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
+
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 app.app_context().push()
 db.create_all()
 
@@ -37,19 +41,21 @@ def home():
 def get_random_cafe():
     cafes = db.session.query(Cafe).all()
     random_cafe = random.choice(cafes)
-    return jsonify(cafe={
-        "id": random_cafe.id,
-        "name": random_cafe.name,
-        "map_url": random_cafe.map_url,
-        "img_url": random_cafe.img_url,
-        "location": random_cafe.location,
-        "seats": random_cafe.seats,
-        "has_toilet": random_cafe.has_toilet,
-        "has_wifi": random_cafe.has_wifi,
-        "has_sockets": random_cafe.has_sockets,
-        "can_take_calls": random_cafe.can_take_calls,
-        "coffee_price": random_cafe.coffee_price,
-    })
+    # Simply convert the random_cafe data record to a dictionary of key-value pairs.
+    return jsonify(cafe=random_cafe.to_dict())
+    # return jsonify(cafe={
+    #     "id": random_cafe.id,
+    #     "name": random_cafe.name,
+    #     "map_url": random_cafe.map_url,
+    #     "img_url": random_cafe.img_url,
+    #     "location": random_cafe.location,
+    #     "seats": random_cafe.seats,
+    #     "has_toilet": random_cafe.has_toilet,
+    #     "has_wifi": random_cafe.has_wifi,
+    #     "has_sockets": random_cafe.has_sockets,
+    #     "can_take_calls": random_cafe.can_take_calls,
+    #     "coffee_price": random_cafe.coffee_price,
+    # })
 
 # HTTP POST - Create Record
 
