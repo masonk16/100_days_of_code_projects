8503b64ae8a166ea7b8c176be10400d10692c41a
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-02-07 20:25:16 +0200

    Add methods to /login route

--- Web Development/blog-with-users/app.py
+++ Web Development/blog-with-users/app.py
@@ -84,7 +84,7 @@ def register():
     return render_template("register.html", form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     """
     Logs in a registered user.
